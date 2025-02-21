6dfc8f147c7a48a9ae72eeb72d06ba052b297b5f
majcke <info.majcke@gmail.com>
2025-02-21 10:23:54 +0100

    A

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //import * as THREE from 'three';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.146.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.146.0/examples/jsm/controls/OrbitControls.js';
 
 //import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 
